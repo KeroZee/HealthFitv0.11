9c41947d8757d8072c797fa4230331dc453b9714
180130D <Blzh1206@gmail.com>
2018-11-28 12:00:08 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
