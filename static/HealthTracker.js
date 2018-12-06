c0153e331e229e1d7f1e72bce8b4413e672ee9d5
ManfredSim <manfredsim2512@gmail.com>
2018-12-06 11:18:37 +0800

    Update html/css/javascript 1.2

---
+++ static/HealthTracker.js
@@ -0,0 +1,7 @@
+function Multiply() {
+    var x = document.getElementById("bpm").value;
+    var y = document.getElementById("update");
+    document.getElementById("record").innerHTML = x * 4;
+    document.getElementById("record2").innerHTML = x * 4;
+}
+
