d248fd410fe1da17ecbc04d4af567f77abe8cfd7
TenshiRachel <joshdebean@gmail.com>
2018-12-29 21:38:23 +0800

    Page styling & minor changes

--- app.py
+++ app.py
@@ -145,8 +145,10 @@ def guide():
     exercises_list = []
     int_list = []
     e1 = Exercises(
-        "A push-up is a common exercise that involves beginning from the prone position,then raising and lowering the body using the arms.",
-        "Pectoral muscles, Abdominal muscles, Triceps",
+        "A Push-up is a common exercise that involves beginning from the prone position,then raising and lowering the body using the arms.",
+        ['Pectoral muscles',
+         'Abdominal muscles',
+         'Triceps'],
         ["Extend your legs backwards and place hands on the ground, slightly more than shoulder-width apart.",
         "Start by bending your elbows to lower your chest until it is just above the floor.",
         "Push yourself back to the starting position. An ideal push-up would be a 1-second push, pause and a 2 second down count.",
@@ -154,8 +156,8 @@ def guide():
         "https://cdn-ami-drupal.heartyhosting.com/sites/muscleandfitness.com/files/styles/full_node_image_1090x614/public/media/1109-pushups_0.jpg?itok=QyFVWqN6",
         "https://www.youtube.com/embed/IODxDxX7oi4")
     e2 = Exercises(
-        'Crunching is an exercise that involves lying face-up on the floor,bending the knees then curling the shoulders towards the waist.',
-        'Abdominal muscles',
+        'Crunching is an exercise that involves lying face-up on the floor, bending the knees then curling the shoulders towards the waist.',
+        ['Abdominal muscles'],
         ['Lie on your back with your knees bent and feet flat on the floor hip-width apart.',
          'Place your hands behind your head such that your thumbs are behind your ears',
          'Hold your elbows to the sides, slightly facing in.',
@@ -168,7 +170,9 @@ def guide():
         "https://www.youtube.com/embed/Xyd_fa5zoEU")
     e3 = Exercises(
         'Jumping Jacks is an exercise that involves jumping with the legs spread wide and hands touching overhead then returning to a position with the feet together and arms at the sides.',
-        'Calve muscles, Shoulder muscles, Hip muscles',
+        ['Calve muscles',
+         'Shoulder muscles',
+         'Hip muscles'],
         ['Stand with your feet together and pointing forward, arms hanging straight at the sides.',
          'In one jump, bend your knees and extend both legs out to the sides while simultaneously extending your arms out to the sides and then up and over the head.',
          'Immediately reverse this motion, jumping back to the starting or neutral standing position.',
@@ -177,7 +181,9 @@ def guide():
         "https://www.youtube.com/embed/UpH7rm0cYbM")
     e4 = Exercises(
         'Tuck jumping is an exercise that involves standing in a shoulder width position, slowly descending into a squat and use your feet to explode off the floor while driving your knees towards your chest.',
-        'Abdominal muscles, Hamstrings, Calve muscles',
+        ['Abdominal muscles',
+         'Hamstrings',
+         'Calve muscles'],
         ['Start in a standing position, slightly bending your knees',
          'Hold your hands out at chest level.',
          'Lower your body quickly into a squatting position, then explode upwards bringing your knees up towards your chest.',
@@ -185,8 +191,10 @@ def guide():
         "http://fitmw.com/wp-content/uploads/2015/07/Exercises-Tuck-Jump.jpg",
         "https://www.youtube.com/embed/ZR6aFqdRi2Y")
     e5 = Exercises(
-        'Squatting is an exercise that involves standing in a shoulder width position, bending your knees all the way down and then explode back up to standing.',
-        'Hip muscles, Hamstrings, Quads',
+        'Squatting is an exercise that involves standing in a shoulder width position, bending your knees all the way down and then suddenly propelling yourself back up to a standing position.',
+        ['Hip muscles',
+         'Hamstrings',
+         'Quads'],
         ['Stand with your feet apart, directly under your hips, and place your hands on your hips.',
          'Standing up tall, put your shoulders back, lift your chest, and pull in your abdominal muscles.',
          'Bend your knees while keeping your upper body as straight as possible, like a sitting position. Lower yourself down as far as you can without leaning your upper body more than a few inches forward.',
@@ -196,7 +204,9 @@ def guide():
         "https://www.youtube.com/embed/aclHkVaku9U")
     e6 = Exercises(
         'Flutter kicks is an exercise that involves lying down face-up, straightening your legs until they are level with your hips and alternating between lifting each leg.',
-        'Abdominal muscles, Hip muscles, Quads',
+        ['Abdominal muscles',
+         'Hip muscles',
+         'Quads'],
         ['Lie on your back with your legs extended and your arms alongside your hips, palms down.',
          'Lift your legs about 4 to 6 inches off the floor. Press your lower back into the floor or gym mat.',
          'Keep your legs straight as you rhythmically raise one leg higher, then switch. Move in a fluttering, up and down motion.',
